ed8a83f4f25876ce39388debdf426b9b5ef21cd7
Michael Ashby <ashby.michael@gmail.com>
2015-11-24 22:43:23 -0800

    define student params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student
 
   def new
     @student = Student.new
@@ -12,5 +13,9 @@ class StudentsController < ApplicationController
   private
     def student_params
       params.require(:student).permit(:name, :contact)
+    end
+
+    def set_student
+      @student = params[:id] if params[:id]
     end
 end
