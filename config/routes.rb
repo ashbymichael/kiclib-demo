ff137210111a0c80fcc19a2166111d2e30709e9a
Michael Ashby <ashby.michael@gmail.com>
2015-11-26 17:17:15 -0800

    add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :students
 
   get  '/login'        => 'session#new'
+  get  '/admin'        => 'application#admin'
   post '/find_student' => 'students#find_student'
   post '/find_book'    => 'books#find_book'
   post '/checkout'     => 'books#checkout'
