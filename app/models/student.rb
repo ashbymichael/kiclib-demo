10e0205964ac75e00c4775b091b2a5e47e95e81e
Michael Ashby <ashby.michael@gmail.com>
2015-11-28 17:31:06 -0800

    write student model tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ActiveRecord::Base
+  validates :name, presence: true
+  validates :contact, presence: true, uniqueness: true
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Student, type: :model do
   it "accepts valid inputs" do
     expect(FactoryGirl.build(:student)).to be_valid
   end
+
+  it "is invalid without a name" do
+    expect(FactoryGirl.build(:student, name: nil)).to_not be_valid
+  end
+
+  it "is invalid without contact info" do
+    expect(FactoryGirl.build(:student, contact: nil)).to_not be_valid
+  end
+
+  it "is invalid with duplicate contact info" do
+    FactoryGirl.create(:student, contact: 'duplicate')
+    expect(FactoryGirl.build(:student, contact: 'duplicate')).to_not be_valid
+  end
 end
