0160e1b0e66a37e1fddae07470ecd7cce728d718
Michael Ashby <ashby.michael@gmail.com>
2015-11-30 14:17:37 -0800

    implement book check-in

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get  '/login'        => 'session#new'
   get  '/admin'        => 'application#admin'
   get  '/checkin'      => 'application#checkin'
+  post '/checkin'      => 'books#checkin'
   post '/find_student' => 'students#find_student'
   post '/find_book'    => 'books#find_book'
   post '/checkout'     => 'books#checkout'
