795e288c42f903b411104dc24d024e36f29623a5
Michael Ashby <ashby.michael@gmail.com>
2015-11-29 16:03:18 -0800

    write user controller tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "#new" do
+    it "assigns a new User to @user" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it "renders :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "#create" do
+    context "with valid attributes" do
+      it "saves the new user to the database" do
+        expect {
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to change(User, :count).by(1)
+
+      end
+
+      it "redirects to root" do
+        post :create, user: FactoryGirl.attributes_for(:user)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new user" do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:invalid_user)
+        }.to_not change(User, :count)
+      end
+
+      it "renders :new template" do
+        post :create, user: FactoryGirl.attributes_for(:invalid_user)
+        expect(response).to redirect_to new_user_path
+      end
+    end
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
+
   factory :user do
     password "password"
     sequence(:username) { |n| "testuser#{n}" }
+
+    factory :invalid_user do
+      username nil
+    end
   end
 
 end
