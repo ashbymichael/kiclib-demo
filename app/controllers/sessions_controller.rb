00e155369bb1ba2ac07cac39fca18b59653bcf48
Michael Ashby <ashby.michael@gmail.com>
2015-12-01 20:27:38 -0800

    clean up sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(:username => params[:session][:username])
-    if (@user && @user.authenticate(params['session']['password']))
-      session[:user_id] = @user.id
+    user = User.find_by(username: params[:session][:username])
+    if (user && user.authenticate(params['session']['password']))
+      session[:user_id] = user.id
+      flash[:notice] = "Hello, #{user.name}!"
       redirect_to root_path
     else
       render 'sessions/new'
