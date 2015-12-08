75dc708afb92a970c878cf7e21953a69f75a44bd
Michael Ashby <ashby.michael@gmail.com>
2015-12-07 16:31:46 -0800

    force student contacts to be saved in lower case

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ActiveRecord::Base
   validates :name, presence: true
   validates :contact, presence: true, uniqueness: true
   has_many :books, dependent: :nullify
+  before_save { contact.downcase! }
 end
