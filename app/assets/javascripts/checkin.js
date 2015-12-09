fb96f03b4215a020fb37195ac87db5fc35295604
Michael Ashby <ashby.michael@gmail.com>
2015-12-08 17:57:02 -0800

    pass check-in tests

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -14,17 +14,36 @@ $(document).ready(function() {
 
       $('#ci_book_input').val('');
       if (Array.isArray(data)) {
-        $('#ci_book_div').prepend(generateBookList(data));
+        $('#ci_book_div').html(generateCheckedOutBookList(data));
       } else if(data.id === undefined) {
-        $('#ci_book_div').prepend("<span class='red'>" +
+        $('#ci_book_div').html("<span class='red'>" +
                                   data.message + "</span>");
         $('#ci_book_id_field').val('');
       } else {
-        $('#ci_book_div').prepend(data.id + " | " + data.title);
-        $('#ci_book_id_field').val(data.id);
+        if(data.student_id !== null) {
+          $('#ci_book_div').html(data.id + " | " + data.title);
+          $('#ci_book_id_field').val(data.id);
+        } else {
+          $('#ci_book_div').html(data.id + " | " + data.title +
+                                 " is not checked out.");
+          $('#ci_book_id_field').val('');
+        }
       }
     });
   });
-
-
 });
+
+var generateCheckedOutBookList = function(data) {
+  var result = "Please select a book: <ul class='red'>";
+  for (var i = 0; i < data.length; i++) {
+    if(data[i].student_id !== null) {
+      result += ("<li><a href='#' data-book=" + data[i].id + ">" + data[i].id +
+                 " | " + data[i].title + "</a></li>");
+    } else {
+      result += ("<li>" + data[i].id + " | " + data[i].title +
+                 " is not checked out." + "</li>");
+    }
+  }
+  result += "</ul>";
+  return result;
+}
--- spec/features/checking_in_books_spec.rb
+++ spec/features/checking_in_books_spec.rb
@@ -34,11 +34,24 @@ feature "Checking in books:" do
       end
     end
 
-    it "tells user if book is not checked out", js: true do
+    it "tells user if book is not checked out with 1 result", js: true do
       @book.check_in_book
       expect(@book.checked_out?).to be(false)
 
       fill_in("book", with: @book.id)
+      click_button("ci_find_book_button")
+
+      within("#ci_book_div") do
+        expect(page).to have_content("#{@book.title} is not checked out.")
+      end
+    end
+
+    it "tells user book is not checked out with multiple results", js: true do
+      @book.check_in_book
+      expect(@book.checked_out?).to be(false)
+      FactoryGirl.create(:book, title: @book.title)
+
+      fill_in("book", with: @book.title)
       click_button("ci_find_book_button")
 
       within("#ci_book_div") do
@@ -52,7 +65,7 @@ feature "Checking in books:" do
       fill_in("book", with: @book.id)
       click_button("ci_find_book_button")
 
-      within("#ci_book_div") do
+      within("#ci_book_info_form_div") do
         click_button("Check in")
       end
 
