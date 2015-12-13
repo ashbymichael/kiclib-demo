a49e7f3d822bad1868657bddec3a407a8a977e52
Michael Ashby <ashby.michael@gmail.com>
2015-12-13 15:47:05 -0800

    add SID to students model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
   validates :name, presence: true
-  validates :contact, presence: true, uniqueness: true
+  validates :sid, presence: true, uniqueness: true
   has_many :books, dependent: :nullify
   before_save { contact.downcase! }
 end
