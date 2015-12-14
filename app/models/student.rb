bce93b88119a3dc84e1ba145cf20860cf240daee
Michael Ashby <ashby.michael@gmail.com>
2015-12-13 19:14:04 -0800

    move before_save to helper method

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,10 @@ class Student < ActiveRecord::Base
   validates :name, presence: true
   validates :sid, presence: true, uniqueness: true
   has_many :books, dependent: :nullify
-  before_save { self.search_name = name.gsub(/\s+/, "").downcase! }
+  before_save :make_searchable_name
+
+  private
+    def make_searchable_name
+      self.search_name = name.gsub(/\s+/, "").downcase!
+    end
 end
